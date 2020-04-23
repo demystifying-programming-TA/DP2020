6f5be6e6f34c670145c00717f33e715a36f0194c
demystifying-programming-TA <aayushi@mit.edu>
2020-04-23 17:58:10 -0400

    Week 2 Updates

---
+++ DemoProject/Other/Session2_Frontend/JS_livecoding/function_googlechart.js
@@ -0,0 +1,63 @@
+// Defining variables & arrays
+// ---------
+// 1. Define variables
+latitude = window.prompt("What longitude are you at?","e.g., -71.0987");
+longitude = window.prompt("What latitude are you at?","e.g., 42.3649");
+
+// 2. Define arrays
+colors = ['#b0120a', '#ffab91']
+columns = ['Country', 'Infections', 'Deaths'];
+
+// Defining functions
+// ---------
+
+// InitializeGraph
+function InitializeGraph() {
+
+  // Make a client-side API call to obtain the user's location
+  $.getJSON("http://ip-api.com/json", function (data, status) {
+
+  // Inspect the data returned from the API
+  console.log(data)
+
+  // Extract the user's longitude and latitude from the data that the API returns
+  user_longitude = data.lon;
+  user_latitude  = data.lat;
+
+  // Inspect the obtained data in the console
+  console.log(user_longitude)
+  console.log(user_latitude)
+
+  // Draw the graph
+  // ** Note: Data is hard-coded > next step: Replace hard-coded data with an API call to the server, i.e., backend 
+  // ** API call specification: Send to server: Longitude, latitude / Receive from server: Corresponding country, # of infections/deaths in that country
+  DrawGraph("United States",852000, 43000);
+
+}
+
+// DrawGraph
+google.charts.load('current', {packages: ['corechart', 'bar']});
+
+function DrawGraph(country, infection, death) {
+
+  // Generate and format the data
+  var data = google.visualization.arrayToDataTable([
+    columns,[country, infection, death]
+  ]);
+
+  // Customize the graph
+  var options = {
+    colors: colors,
+    chartArea: {width: '50%'},
+    hAxis: {
+      title: 'Number of infections and deaths'
+    }
+  };
+
+  // Generate the graph
+  var chart = new google.visualization.BarChart(document.getElementById('chart'));
+
+  // Display the graph
+  chart.draw(data, options);
+
+}
---
+++ DemoProject/Other/Session2_Frontend/JS_livecoding/function_jquery.js
@@ -0,0 +1,50 @@
+// Defining variables & arrays
+// ---------
+// 1. Define variables
+latitude = window.prompt("What longitude are you at?","e.g., -71.0987");
+longitude = window.prompt("What latitude are you at?","e.g., 42.3649");
+
+// 2. Define arrays
+colors = ['#b0120a', '#ffab91']
+columns = ['Country', 'Infections', 'Deaths'];
+
+// Defining functions
+// ---------
+
+// InitializeGraph
+function InitializeGraph() {
+
+  // Make a client-side API call to obtain the user's location
+  $.getJSON("http://ip-api.com/json", function (data, status) {
+
+  // Inspect the data returned from the API
+  console.log(data)
+
+  // Extract the user's longitude and latitude from the data that the API returns
+  user_longitude = data.lon;
+  user_latitude  = data.lat;
+
+  // Inspect the obtained data in the console
+  console.log(user_longitude)
+  console.log(user_latitude)
+
+}
+
+// DrawGraph
+function InitializeGraph() {
+
+  // Make a client-side API call to obtain the user's location
+  $.getJSON("http://ip-api.com/json", function (data, status) {
+
+  // Inspect the data returned from the API
+  console.log(data)
+
+  // Extract the user's longitude and latitude from the data that the API returns
+  user_longitude = data.lon;
+  user_latitude  = data.lat;
+
+  // Inspect the obtained data in the console
+  console.log(user_longitude)
+  console.log(user_latitude)
+
+}
